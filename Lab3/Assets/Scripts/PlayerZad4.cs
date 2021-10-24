49c18029abb70a0b4542304d8d0d609a9169a433
mskwarska <150536@student.uwm.edu.pl>
2021-10-24 22:11:38 +0200

    zad 4

---
+++ Lab3/Assets/Scripts/PlayerZad4.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerZad4 : MonoBehaviour
+{
+    public float speed = 1.5f;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+        transform.position += moveDirection * speed;
+
+    }
+    
+    void OnCollision(Collision col)
+    {
+        if (col.gameObject.tag == "obstacle")
+        {
+            Debug.Log("Collision Detected");
+        }
+
+    }
+}
